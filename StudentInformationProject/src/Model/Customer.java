96e197ea852d906cc222c4f8e1bc26345da6f91c
sanket singhal <sanket singhal@ASUS-VIVOBOOK>
2022-07-13 12:59:47 +0530

    Adding the file

--- StudentInformationProject/src/Model/Customer.java
+++ StudentInformationProject/src/Model/Customer.java
@@ -1,10 +1,20 @@
 package Model;
 
 public class Customer {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int customerNo;
+	String sName;
+	int customerAge;
+	
+	public int getCustomerNo() {
+		return customerNo;
 	}
-
+	public void setCustomerNo(int customerNo) {
+		this.customerNo = customerNo;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
 }
