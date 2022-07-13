d9722ae575f0bd9e7c4e02acd75d30701349a345
sanket singhal <sanket singhal@ASUS-VIVOBOOK>
2022-07-13 13:43:05 +0530

    NewAccount

--- StudentInformationProject/src/Model/Account.java
+++ StudentInformationProject/src/Model/Account.java
@@ -1,5 +1,19 @@
 package Model;
 
 public class Account {
-
+	int accountNo;
+	double balance;
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 }
